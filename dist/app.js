751f1d9db6df09654bc0534426ab189c5c2013c1
aardaipek <ardaipek66@gmail.com>
2021-11-28 14:22:21 +0300

    cors added

--- dist/app.js
+++ dist/app.js
@@ -7,9 +7,16 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const routes_1 = __importDefault(require("./routes/routes"));
 const PORT = process.env.PORT || 4200;
+let cors = require('cors');
 const app = (0, express_1.default)();
+app.use(cors({
+    credentials: true,
+    origin: true
+}));
+app.options('*', cors());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
+app.get('/', (req, res) => res.send('Working!!!'));
 app.use('/api', routes_1.default);
 app.listen(PORT, () => console.log("server running on :" + PORT));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes/routes';
 const PORT = process.env.PORT || 4200;
+let cors = require('cors');
 const app = express();
 
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.options('*', cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Working!!!'));
 
 app.use('/api', routes);
 
